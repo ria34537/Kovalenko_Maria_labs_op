7e61ac0e0c73aea1acaafa9583f393ed79d2d4c6
Ria34537 <m.kovalenko.ip15@kpi.ua>
2021-10-13 23:18:01 +0300

    Добавьте файлы проекта.

---
+++ lab_1_c++/lab_1_c++.cpp
@@ -0,0 +1,25 @@
+﻿
+// 12. Задана довжина окола. Визначити площу кола обмеженого цим околом
+
+#include <math.h>
+#include <iomanip>
+#include <iostream>
+
+
+
+using namespace std;
+
+
+int main()
+{
+    float length, square; // задаємо змінні для довжини та площі кола
+    const float pi = 3.1459265358979; //визначаємо пі
+    setlocale(LC_ALL, "");
+    cout << "12. Задана довжина окола. Визначити площу кола обмеженого цим околом";
+    cout << "\n Будь ласка, введіть довжину кола ";
+    cin >> length; // вводимо довжину кола
+    square = (length * length) / (pi) ; //знаходимо площу кола
+    cout << "Площа кола, обмеженого околом = " << fixed << setprecision(3) << square; // виводимо площу кола
+
+    return 0;
+}
---
+++ lab_2/lab_2.cpp
@@ -0,0 +1,62 @@
+﻿
+// 12. Визначити якому квалранту належть точка з координатами x,y
+
+#include <math.h>
+#include <iomanip>
+#include <iostream>
+
+
+
+using namespace std;
+
+
+int main()
+{
+    int x, y;
+    string answer = " ";
+    setlocale(LC_ALL, "");
+    cout << "12. Визначити якому квадранту належть точка з координатами x,y";
+    cout << "\n Будь ласка, x ";
+    cin >> x; // вводимо x
+    cout << "\n Будь ласка,  y ";
+    cin >> y; // вводимо y
+
+
+
+
+    if (x==0 or y==0)
+        answer = "Точка знаходиться на координатнiй осi";
+    else {
+       
+        if (x > 0) {
+
+            if (y > 0) 
+                answer = "Точка в першому квадрантi";
+            else
+                answer = "Точка в четвертому квадрантi";
+            
+        }
+        else{
+                if (y > 0)
+                    answer = "Точка в другому квадрантi";
+                else
+                    answer = "Точка в третьому квадрантi";
+         }
+        
+            
+            
+    }
+
+    cout << answer;
+
+
+
+
+
+
+
+
+
+
+
+}
