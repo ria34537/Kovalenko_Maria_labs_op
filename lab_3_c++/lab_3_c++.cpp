43ccbe29551b4e1fa953be33bc1136e9c3935b5c
Ria34537 <m.kovalenko.ip15@kpi.ua>
2021-10-20 12:16:18 +0300

    test

---
+++ lab_3_c++/lab_3_c++.cpp
@@ -0,0 +1,69 @@
+﻿
+// 12. Визначити якому квалранту належть точка з координатами x,y
+
+#include <math.h>
+#include <iomanip>
+#include <iostream>
+
+
+
+
+using namespace std;
+
+
+int main()
+{
+    
+    float n, temp, sinx, x, eps;
+
+    setlocale(LC_ALL, "");
+    cout << "12. Визначити якому квадранту належть точка з координатами x,y";
+    cout << "\n Будь ласка, x ";
+    cin >> x; // вводимо x
+    cout << "\n Будь ласка,  точність ";
+    cin >> eps; // вводимо y
+
+
+    temp = x;
+    sinx = temp;
+    n = 1;
+
+    while (fabs(temp) > eps)
+    {
+        temp = temp * (-1 * (x * x) /((2*n + 1)*(2*n )));
+        sinx += temp;
+        ++n;
+
+
+    }
+
+    cout << sinx;
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
